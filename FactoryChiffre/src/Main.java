b6c444e590f7e5bc4f932e75879d4128551ff0d8
Peter Ruß <91833328+Peter-Russ@users.noreply.github.com>
2024-02-09 10:55:22 +0100

    --

--- FactoryChiffre/src/Main.java
+++ FactoryChiffre/src/Main.java
@@ -1,20 +1,8 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String sDisplay = "";
-        for (int i = 0; i < ChiffreFactory.getChiffreList().length; i++) {
-            sDisplay += ChiffreFactory.getChiffreList()[i] + "(" + i + ")\n";
-        }
 
-        int iSelectedAlgo = Integer.parseInt(JOptionPane.showInputDialog(sDisplay + "Wählen Sie einen Algorithmus:"));
-        String myKey = JOptionPane.showInputDialog("Key:");
-        String klartext = JOptionPane.showInputDialog("Klartext:");
-
-        ChiffreText myChiffre = ChiffreFactory.getChiffre(iSelectedAlgo, myKey);
-        myChiffre.setKlartext(klartext);
-
-        // Hier würde normalerweise die Verschlüsselung durchgeführt und das Ergebnis ausgegeben
+        
     }
 }
